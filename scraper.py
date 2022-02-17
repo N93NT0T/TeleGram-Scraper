5d22bd406ff8f413a5c02220a9d63521de32c251
N93NT0T <75997866+N93NT0T@users.noreply.github.com>
2022-02-17 23:32:34 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
-from telethon import TelegramClient
+from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
-
 import os, sys
 import configparser
 import csv
@@ -21,26 +20,32 @@ def banner():
 youtube.com/channel/UCnknCgg_3pVXS27ThLpw3xQ
         """)
 
-if os.path.isfile('N93NT0T_log.txt'):
-    with open('N93NT0T_log.txt', 'r') as r:
-        data = r.readlines()
-    api_id = data[0]
-    api_hash = data[1]
+cpass = configparser.RawConfigParser()
+cpass.read('config.data')
 
-else:
-    api_id = input('Enter api_id: ')
-    api_hash = input('Enter api_hash: ')
-    with open('N93NT0T_log.txt', 'w') as a:
-        a.write(api_id + '\n' + api_hash)
+try:
+    api_id = cpass['cred']['id']
+    api_hash = cpass['cred']['hash']
+    phone = cpass['cred']['phone']
+    client = TelegramClient(phone, api_id, api_hash)
+except KeyError:
+    os.system('clear')
+    banner()
+    print(re+"[!] run python3 setup.py first !!\n")
+    sys.exit(1)
 
-client = TelegramClient('N93NT0T', api_id, api_hash)
-
-
-
-async def main():
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    os.system('clear')
+    banner()
+    client.sign_in(phone, input(gr+'[+] Enter the code: '+re))
+ 
+os.system('clear')
+banner()
 chats = []
 last_date = None
-chunk_size = 2000
+chunk_size = 200
 groups=[]
  
 result = client(GetDialogsRequest(
@@ -76,9 +81,9 @@ all_participants = client.get_participants(target_group, aggressive=True)
  
 print(gr+'[+] Saving In file...')
 time.sleep(1)
-with open(group.title + ".csv","w",encoding='UTF-8') as f:
+with open(group.id + "_members.csv","w",encoding='UTF-8') as f:
     writer = csv.writer(f,delimiter=",",lineterminator="\n")
-    writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
+    writer.writerow(['userid','username','name','access hash','group id'])
     for user in all_participants:
         if user.username:
             username= user.username
@@ -93,5 +98,5 @@ with open(group.title + ".csv","w",encoding='UTF-8') as f:
         else:
             last_name= ""
         name= (first_name + ' ' + last_name).strip()
-        writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id])      
+        writer.writerow([user.id,username,name,user.access_hash, target_group.id])      
 print(gr+'[+] Members scraped successfully.')
